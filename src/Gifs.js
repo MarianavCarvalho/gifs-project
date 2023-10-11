edbb69a65bba8d17a53be43aaf5d9bec37fd8bcd
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-10-11 22:22:07 +0100

    added search engine started api integration

--- src/Gifs.js
+++ src/Gifs.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gifs.css";
+import axios from "axios";
 
 export default function Gifs(){
+ let[word, setWord] = useState(null);
 
-    function handleSubmit(event){
-event.preventDefault();
+    function handleResponse(response){
+        console.log(response.data)
     }
 
-    return(
-    <div>
+    function search(){
+        let apiGifsKey = "4FcQMZlVD6y1yqugPhiadllXoAvePUgE"
+let apiUrl = `https://api.giphy.com/v1/gifs/search/tags?api_key=${apiGifsKey}&q=giphy&limit=25&offset=0`;
+axios.get(apiUrl).then(handleResponse);
+    }
+    
+     function handleSubmit(event){
+event.preventDefault();
+search()
+    }
+
+     function handleWordChange(event){
+setWord(event.target.value);
+ }
+
+return(
+    <div className="Gifs">
 <form onSubmit={handleSubmit}>
-    <input type="search"></input>
+    <input type="search" onChange={handleWordChange}/>
 </form>
     </div>)
 }
