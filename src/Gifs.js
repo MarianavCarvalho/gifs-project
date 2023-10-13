ba7f3f2d86aec42e0af2700e87dd5987b4468b69
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-10-13 22:36:08 +0100

    updated API url and created results.js

--- src/Gifs.js
+++ src/Gifs.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import "./Gifs.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Gifs(){
  let[word, setWord] = useState(null);
+ let [results, setResults] = useState(null);
 
     function handleResponse(response){
-        console.log(response.meta)
+        setResults(response.data.data[1])
     }
 
     // https://developers.giphy.com/explorer/
     function search(){
         let apiGifsKey = "4FcQMZlVD6y1yqugPhiadllXoAvePUgE"
-let apiUrl = `https://api.giphy.com/v1/gifs/search/${word}?api_key=${apiGifsKey}&q=giphy&limit=25&offset=0`;
+let apiUrl = `https://api.giphy.com/v1/channels/search?api_key=${apiGifsKey}&q=${word}&limit=25&offset=0`;
 axios.get(apiUrl).then(handleResponse);
     }
     
@@ -27,8 +29,17 @@ setWord(event.target.value);
 
 return(
     <div className="Gifs">
-<form onSubmit={handleSubmit}>
+        <section>
+            <h1>What GIF do you want to look up?</h1>
+            <form onSubmit={handleSubmit}>
     <input type="search" onChange={handleWordChange}/>
 </form>
+            <div className="hint">
+                suggested words: sunset, snow, plant, yoga...
+            </div>
+            </section>
+
+      <Results results={results}/>  
+
     </div>)
 }
---
+++ src/Results.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Results(props){
+    console.log(props.results)
+    if (props.results){
+return (
+    <div className="Results"> "Results"</div>
+)
+    } else {
+        return null;
+    }
+    
+}
