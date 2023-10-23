efe42e056aaaf9b33186224618e7e6e8b8f64f4c
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-10-23 19:53:43 +0100

    added css to results

--- src/Results.js
+++ src/Results.js
@@ -1,18 +1,18 @@
 
 import React from "react";
+import "./Results.css";
 
 
 export default function Results(props){
-  console.log(props.results)
      if (props.results){
 return (
     <div className="Results">
  <div class="row"> 
       {props.results.map(function(result, index){
         return (
-    <div class="col-2" key={index}>
+    <div class="col-4" key={index}>
           
-        <img src={result.images.fixed_height.url} alt=""/>
+        <img src={result.images.fixed_height.url} alt="" className="img-responsive"/>
         </div>
        
         )
